2369a3becc10b4ec3b252601e981a7d6bc3b28ea
Dmitrii Okunev <xaionaro@dx.center>
2025-08-19 21:36:10 +0100

    Add CompareAndSwapPointer and SwapPointer

--- pointer.go
+++ pointer.go
@@ -25,3 +25,26 @@ func LoadPointer[T any](src **T) *T {
 		(*unsafe.Pointer)(unsafe.Pointer(src)),
 	))
 }
+
+// CompareAndSwapPointer atomically compares and swaps a pointer to a value.
+// This is just a type-safe wrapper for atomic.CompareAndSwapPointer.
+//
+// Consider also using atomic.Pointer, instead of this function.
+func CompareAndSwapPointer[T any](addr **T, old, new *T) bool {
+	return atomic.CompareAndSwapPointer(
+		(*unsafe.Pointer)(unsafe.Pointer(addr)),
+		unsafe.Pointer(old),
+		unsafe.Pointer(new),
+	)
+}
+
+// CompareAndSwapPointer atomically compares and swaps a pointer to a value.
+// This is just a type-safe wrapper for atomic.SwapPointer.
+//
+// Consider also using atomic.Pointer, instead of this function.
+func SwapPointer[T any](addr **T, new *T) *T {
+	return (*T)(atomic.SwapPointer(
+		(*unsafe.Pointer)(unsafe.Pointer(addr)),
+		unsafe.Pointer(new),
+	))
+}
